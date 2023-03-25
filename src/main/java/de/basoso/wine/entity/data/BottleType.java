449bcd1e6fab9d23bb0da0e2e997a3ec9a643383
Michael Eimer <michael.eimer@outlook.com>
2023-03-26 00:05:48 +0100

    BottleType is a class now

--- src/main/java/de/basoso/wine/entity/data/BottleType.java
+++ src/main/java/de/basoso/wine/entity/data/BottleType.java
@@ -1,8 +1,28 @@
 package de.basoso.wine.entity.data;
 
-public enum BottleType {
-    PICCOLO,
-    BOUTEILLE,
-    MAGNUM,
-    DOPPELMAGNUM,
+import de.basoso.wine.entity.model.Wine;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "bottle")
+public class BottleType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    @OneToMany
+    private List<Wine> wines = new ArrayList<>();
+
 }
