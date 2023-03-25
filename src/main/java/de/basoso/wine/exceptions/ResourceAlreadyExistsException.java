fedc89d2d7ac452ea1a251ccd19847cfebfeb453
Michael Eimer <michael.eimer@outlook.com>
2023-03-26 00:05:29 +0100

    ReasourceAlreadyExists added

---
+++ src/main/java/de/basoso/wine/exceptions/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package de.basoso.wine.exceptions;
+
+public class ResourceAlreadyExistsException extends RuntimeException{
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
